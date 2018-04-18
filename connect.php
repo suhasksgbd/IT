3fde68231ac62ecd599d6effc9d7e1afef1d91bd
suhasksgbd <38484840+suhasksgbd@users.noreply.github.com>
2018-04-18 09:51:47 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$host="localhost";
+    $user="root";
+    $password="tiger";
+    $dbname="election";
+    $con=mysqli_connect($host,$user,$password,$dbname) or die("could not connect to the database");
+?>
